e2e209f5182ea8f4981c054be72a122e3565b6c7
nekotxs <68693406+nekotxs@users.noreply.github.com>
2020-11-28 21:04:09 +0700

    Предварительная версия заявки на перевозку

---
+++ app/src/main/java/com/kingofraccoon/zik/request/Request.java
@@ -0,0 +1,103 @@
+package com.kingofraccoon.zik.request;
+
+import java.util.Date;
+
+import com.kingofraccoon.zik.users.User;
+
+public class Request {
+    private CargoStatus status;
+
+    final private byte departurePoint;
+    final private byte receptionPoint;
+
+    final private User initiator;
+    final private User creator;
+    final User cargoSender;
+    final User cargoRecipient;
+    User driver;
+
+    final private Date createTime;
+    private Date arriveTime;
+
+    public Request(User initiator, User creator, User cargoSender, User cargoRecipient, byte departurePoint, byte receptionPoint) {
+        this.departurePoint = departurePoint;
+        this.receptionPoint = receptionPoint;
+
+        this.initiator = initiator;
+        this.creator = creator;
+        this.cargoSender = cargoSender;
+        this.cargoRecipient = cargoRecipient;
+
+        createTime = new Date();
+
+        status = CargoStatus.CREATED;
+    }
+
+    //Сеттеры лучше не использовать, т.к. статус отправки не учитывается
+
+    public long getNumber() {
+        return createTime.getTime();
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public Date getArriveTime() {
+        return arriveTime;
+    }
+
+    public byte getDeparturePoint() {
+        return departurePoint;
+    }
+
+    public byte getReceptionPoint() {
+        return receptionPoint;
+    }
+
+    public CargoStatus getStatus() {
+        return status;
+    }
+
+    public User getInitiator() {
+        return initiator;
+    }
+
+    public User getCreator() {
+        return creator;
+    }
+
+    public User getCargoSender() {
+        return cargoSender;
+    }
+
+    public User getCargoRecipient() {
+        return cargoRecipient;
+    }
+
+    public User getDriver() {
+        return driver;
+    }
+
+    public void approve(User driver, Date arriveTime) {
+        this.driver = driver;
+        status = CargoStatus.ACCEPTED;
+        if (arriveTime.equals(createTime)) {
+            this.arriveTime = new Date(createTime.getTime() + 600000);
+        } else {
+            this.arriveTime = arriveTime;
+        }
+    }
+
+    public void accept() {
+        status = CargoStatus.EXECUTING;
+    }
+
+    public void cancel(String reason, String comment) {
+
+    }
+
+    public void done() {
+        status = CargoStatus.FINISHED;
+    }
+}
