fe49146900e7834082c55208a116542346f02091
nekotxs <68693406+nekotxs@users.noreply.github.com>
2020-11-28 23:56:58 +0700

    Теперь это класс

--- app/src/main/java/com/kingofraccoon/zik/users/User.java
+++ app/src/main/java/com/kingofraccoon/zik/users/User.java
@@ -1,21 +1,55 @@
 package com.kingofraccoon.zik.users;
 
-public interface User {
+public class User {
+    private String name;
+    private int subunitNum;
+    private String phone;
+    private UserAccessLevel userAccessLevel;
+
+    User(String name, int subunitNum, String phone, UserAccessLevel userAccessLevel) {
+        this.name = name;
+        this.subunitNum = subunitNum;
+        this.phone = phone;
+        this.userAccessLevel = userAccessLevel;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
     /**
      *
      * @return Возвращает имя пользователя
      */
-    String getName();
+    public String getName(){
+        return name;
+    }
 
+    public void setSubunitNum(int subunitNum) {
+        this.subunitNum = subunitNum;
+    }
     /**
      *
      * @return Возвращает номер подразделения пользователя
      */
-    int getSubunit();
+    public int getSubunit() {
+        return subunitNum;
+    }
 
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
     /**
      *
      * @return Возвращает номер телефона пользвателя
      */
-    String getPhone();
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setUserAccessLevel(UserAccessLevel userAccessLevel) {
+        this.userAccessLevel = userAccessLevel;
+    }
+    public UserAccessLevel getUserAccessLevel() {
+        return userAccessLevel;
+    }
 }
